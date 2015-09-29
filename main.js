b07a11d9f249bef555c8920417b4589b6bede901
Maroun Rizk <rizk74@hotmail.com>
2015-09-29 15:32:52 +1000

    dgdgdgdgdgdgiwpaotiuephfipahftahsdfgas;gdzftgae;zdergekduthdkh.krdf.kae.kd.tsd

--- main.js
+++ main.js
@@ -170,8 +170,8 @@ function endGame(deltaTime){
 	drawMap();
 	context.font = "50px Impact";
 	context.fillStyle = "red";
-	var textMeasure = context.measureText("You died");
-	context.fillText("You died", SCREEN_WIDTH/2 - (textMeasure.width/2), SCREEN_HEIGHT/2);
+	var textMeasure = context.measureText("HAH, YOU SUCK.");
+	context.fillText("HAH, YOU SUCK", SCREEN_WIDTH/2 - (textMeasure.width/2), SCREEN_HEIGHT/2);
 
 
 
